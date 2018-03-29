586d60e7c66f96b78bab46beb42a8a2ce44e6476
davidbullado <david.bullado@gmail.com>
2018-03-29 11:28:38 +0200

    Update with V2

--- Check-Pwned-Password.ps1
+++ Check-Pwned-Password.ps1
@@ -1,43 +1,92 @@
-$response = Read-host "Password ?" -AsSecureString
- 
-$StringBuilder = New-Object System.Text.StringBuilder
-[System.Security.Cryptography.HashAlgorithm]::Create("SHA1").ComputeHash([System.Text.Encoding]::UTF8.GetBytes([Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($response)))) | foreach{
-    [Void]$StringBuilder.Append($_.ToString("x2"))
-}
-$Hash = $StringBuilder.ToString()
- 
-$Hash_Prefix = $Hash.Substring(0,5)
-$Uri = "https://api.pwnedpasswords.com/range/$Hash_Prefix"
- 
-add-type @"
-using System.Net;
-using System.Security.Cryptography.X509Certificates;
-public class TrustAllCertsPolicy1 : ICertificatePolicy {
-   public bool CheckValidationResult(
-       ServicePoint srvPoint, X509Certificate certificate,
-       WebRequest request, int certificateProblem) {
-       return true;
-   }
-}
-"@
-$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
-[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
-[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy1
- 
-$ProfileResponse = Invoke-WebRequest -Uri $Uri
- 
-$suffix = $Hash.Substring(5,$Hash.Length-5).ToUpper()
- 
-if ($ProfileResponse.Content -match $suffix)
+[CmdletBinding()]
+param(
+    [Parameter(ValueFromPipeline)]
+    [string]
+    $password_notsecure,
+
+    [Parameter(ValueFromPipeline)]
+    [SecureString]
+    $password_secure
+)
+
+begin
 {
    
-    $pline = $ProfileResponse.Content -split "`n" | Select-String -Pattern $suffix
-    $nbPwned = ($pline -split ":")[1] -replace "`r" , ''
-    $result = "Oh no -- pwned $nbPwned time(s)!"
+    add-type @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+    public bool CheckValidationResult(
+        ServicePoint srvPoint, X509Certificate certificate,
+        WebRequest request, int certificateProblem) {
+        return true;
+    }
 }
-else
+"@
+    [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+    $sha1 = [System.Security.Cryptography.HashAlgorithm]::Create("SHA1")
+    $secProtocol = [System.Net.ServicePointManager]::SecurityProtocol
+    $NbPass = 0
+    $IsReadHost = $False
+
+
+}
+process
 {
-    $result = "Seems OK!"
+
+    $NbPass++
+
+    if (!$password_secure -and !$password_notsecure) {
+        $IsReadHost = $True
+        $password_secure = Read-host "Password ?" -AsSecureString
+    }
+
+    if (!$password_notsecure) {
+        $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password_secure)
+        $password_notsecure = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
+        $bstr = $null
+    }
+
+    $StringBuilder = New-Object System.Text.StringBuilder
+
+    $sha1.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($password_notsecure)) | foreach{
+        [Void]$StringBuilder.Append($_.ToString("x2")) 
+    } 
+
+    $password_notsecure = ""
+    $Hash = $StringBuilder.ToString() 
+
+    $Uri = "https://api.pwnedpasswords.com/range/"+$Hash.Substring(0,5)
+
+    [System.Net.ServicePointManager]::SecurityProtocol = 'Ssl3,Tls,Tls11,Tls12'
+    
+    $suffix = $Hash.Substring(5).ToUpper()
+
+    if ((iwr $Uri).Content -match "${suffix}:(\d+)")
+    {
+        $hits = $matches[1]
+        $result = "Oh no -- pwned $hits time(s)!"
+    }
+    else
+    {
+        $result = "Seems OK!"
+    }
+
+    if ($IsReadHost)
+    {
+        $wshell = New-Object -ComObject Wscript.Shell
+        [void]$wshell.Popup($result,0,"Password checked",0x1)
+    }
+    else
+    {
+        @{
+            ID    = $NbPass
+            Pwned = $True
+            Times = $hits
+        }
+    }
 }
-$wshell = New-Object -ComObject Wscript.Shell
-$wshell.Popup($result,0,"Password checked",0x1)
+end
+{
+    [System.Net.ServicePointManager]::SecurityProtocol = $secProtocol
+}
